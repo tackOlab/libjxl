ad7d018ea53b354d8824d7fb3936afb2987210f2
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-02-27 17:03:51 +0900

    test script improved and myrand() is modified

--- encrypt_test.sh
+++ encrypt_test.sh
@@ -2,6 +2,10 @@
 
 EFFORT=${2:-7}
 
+if [ $EFFORT -gt 7 ]; then
+	echo "**** WARNING **** effort value greater than 7 will not be losslessly decryptable."
+fi
+
 DISTANCE[1]=0.01
 DISTANCE[2]=0.05
 DISTANCE[3]=0.1
@@ -20,16 +24,19 @@ PSNRCMD=~/Documents/Clone/WG1/OpenHTJ2K/build-relwithdebinfo/bin/imgcmp
 ENC=build/tools/cjxl
 DEC=build/tools/djxl
 
-printf "-distance, size of encrypted, size of normal (in Bytes)\n"
+printf "-distance, encrypted.jxl (Bytes), normal.jxl (Bytes), diff(percent)\n"
 for d in ${DISTANCE[@]}
 do
-	printf $d
-	printf ", "
+#	printf $d
+#	printf ", "
 	ENCOUT=work/out$d.jxl
 	NOENCOUT=work/noenc$d.jxl
-	$ENC $1 $ENCOUT --pencrypt --num_threads=0 -d $d -e $EFFORT --quiet
-	printf ", "
-	$ENC $1 $NOENCOUT --num_threads=0 -d $d -e $EFFORT --quiet
+	SIZE_E=`$ENC $1 $ENCOUT --pencrypt --num_threads=0 -d $d -e $EFFORT --quiet`
+#	printf ", "
+	SIZE_N=`$ENC $1 $NOENCOUT --num_threads=0 -d $d -e $EFFORT --quiet`
+	DIFF=$(( $SIZE_E - $SIZE_N ))
+	PERCENT=`echo "scale=5; 100 * $DIFF / $SIZE_N" | bc`
+	printf "%6.2f %8d, %8d, %8.4f" $d $SIZE_E $SIZE_N $PERCENT
 	printf "\n"
 done
 
@@ -38,7 +45,7 @@ printf "\n"
 printf "* Errors between encrypted and original (PAE,MSE,PSNR):\n"
 for d in ${DISTANCE[@]}
 do
-	printf $d
+	printf "%6.2f" $d
 	printf ", "
 	ENCOUT=work/out$d.jxl
 	DECENCOUT=work/enc$d.ppm
@@ -51,7 +58,7 @@ printf "\n"
 printf "* Errors between decrypted and non-encrypted (PAE,MSE,PSNR):\n"
 for d in ${DISTANCE[@]}
 do
-	printf $d
+	printf "%6.2f" $d
 	printf ", "
 	ENCOUT=work/out$d.jxl
 	NOENCOUT=work/noenc$d.jxl
@@ -67,7 +74,7 @@ printf "\n"
 printf "* Errors between original and non-encrypted (PAE,MSE,PSNR):\n"
 for d in ${DISTANCE[@]}
 do
-	printf $d
+	printf "%6.2f" $d
 	printf ", "
 	NOENCOUT=work/noenc$d.jxl
 	DECNOENCOUT=work/noenc$d.ppm
