bc9f40f7dbd3e82ee2c63dec4e9f7013a33c7a76
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-03-13 10:33:04 +0900

    update cmake script

--- make_build.sh
+++ make_build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+export CMAKE_PREFIX_PATH=`brew --prefix giflib`:`brew --prefix jpeg-turbo`:`brew --prefix libpng`:`brew --prefix zlib`
+
 cmake -B./build -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++ -DCMAKE_C_COMPILER=/opt/homebrew/opt/llvm/bin/clang -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -G Ninja -DCMAKE_LINKER=ld.lld
